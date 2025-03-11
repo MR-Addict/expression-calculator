1d48167596c88e5b3db8d05baba49dec5bbb667e
Cael <MR-Addict@qq.com>
2025-03-11 16:22:42 +0800

    feat: add examples

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,17 +1,30 @@
 import clsx from "clsx";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import style from "./home.module.css";
 
+import { examples } from "@/data/examples";
 import { expressionParse } from "@/lib/parser";
 import { ParseResultType } from "@/lib/parser/type";
 
 export default function Home() {
-  const [expression, setExpresion] = useState("( 1.1 + 2.2 ) * 3.3 - 4.4 * ( 5 / 6 )");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [expression, setExpresion] = useState(examples[0]);
   const [result, setResult] = useState<ParseResultType>(expressionParse(expression));
 
   function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
     setExpresion(e.target.value);
+  }
+
+  function fouceTextarea() {
+    if (!textareaRef.current) return;
+    textareaRef.current.focus();
+    textareaRef.current.setSelectionRange(textareaRef.current.value.length, textareaRef.current.value.length);
+  }
+
+  function handleSetExample(example: string) {
+    setExpresion(example);
+    setTimeout(fouceTextarea, 0);
   }
 
   useEffect(() => {
@@ -20,18 +33,39 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, [expression]);
 
+  useEffect(fouceTextarea, []);
+
   return (
     <section aria-label="Home" className={style.wrapper}>
-      <textarea
-        value={expression}
-        onChange={handleChange}
-        className={clsx(style.container, style.input)}
-        placeholder="Input raw expression here..."
-      />
+      <div className="flex flex-col gap-4">
+        <div className={style.section}>
+          <h1 className={style.title}>Examples</h1>
+          <ul className={clsx(style.examples, style.container)}>
+            {examples.map((exp) => (
+              <li key={exp}>
+                <button type="button" onClick={() => handleSetExample(exp)}>
+                  {exp}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className={style.container}>
-        <h2>Expression result:</h2>
-        <pre>{JSON.stringify(result, null, 2)}</pre>
+        <div className={style.section}>
+          <h1 className={style.title}>Expression</h1>
+          <textarea
+            ref={textareaRef}
+            value={expression}
+            onChange={handleChange}
+            className={clsx(style.container, style.input)}
+            placeholder="Input raw expression here..."
+          />
+        </div>
+      </div>
+
+      <div className={clsx(style.result, style.section)}>
+        <h2>Result</h2>
+        <pre className={style.container}>{JSON.stringify(result, null, 2)}</pre>
       </div>
     </section>
   );
